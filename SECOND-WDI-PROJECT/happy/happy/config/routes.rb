82bc85da1565aebd348c33279a54ad9d048ef341
joeyaza <JAzadehdel@gmail.com>
2015-11-02 19:31:31 +0000

    cosmetics improvements

--- SECOND-WDI-PROJECT/happy/happy/config/routes.rb
+++ SECOND-WDI-PROJECT/happy/happy/config/routes.rb
@@ -15,7 +15,7 @@ Rails.application.routes.draw do
 
   resources :comments
 
-  root "pages#index"
+  root "pics#index"
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
